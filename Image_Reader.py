d9f842437f96634190034469263b14ce0ab9e036
Rounak Singh <rounaksingh9431738764@gmail.com>
2022-11-12 22:26:37 +0530

    Changed Directories System

--- Image_Reader.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-img = cv2.imread("Image/butterfly.jpg")
-cv2.imshow("Display Image", img)
-gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-cv2.imshow("Gray Scale Image", gray_img)
-cv2.waitKey(0)
