c0707db27c8de4406594168f7d00011143592492
Rounak Singh <rounaksingh9431738764@gmail.com>
2022-12-19 19:15:55 +0530

    Completed Project: Assigned To Make a Model using Tensorflow Which Detects Rock, Paper, Scissors

--- Code/detect_rock_game.py
+++ Code/detect_rock_game.py
@@ -2,35 +2,36 @@ import cv2
 import tensorflow as tf
 import numpy as np
 
-model = tf.keras.models.load_model("../DataSet/keras_model.h5")
+model = tf.keras.models.load_model("../DataSet/keras_model_for_rock.h5")
 
-
-# define a video capture object
 vid = cv2.VideoCapture(0)
 
-while (True):
-
-    # Capture the video frame by frame
+while True:
     ret, frame = vid.read()
+
+    frame = cv2.flip(frame, 1)
 
     image = cv2.resize(frame, (224, 224))
     test_image = np.array(image, dtype=np.float32)
     test_image = np.expand_dims(test_image, 0)
     normalized_image = test_image/255.0
     prediction = model.predict(normalized_image)
+
     print(prediction)
 
-    # Display the resulting frame
+    # rock = int(prediction[0][0]*100)
+    # paper = int(prediction[0][1]*100)
+    # scissor = int(prediction[0][2]*100)
+
+    # print(f"Rock: {rock} %, Paper: {paper} %, Scissor: {scissor} %")
+
     cv2.imshow('frame', frame)
 
-    # Quit window with spacebar
     key = cv2.waitKey(1)
 
     if key == 32:
         break
 
-# After the loop release the cap object
 vid.release()
 
-# Destroy all the windows
 cv2.destroyAllWindows()
